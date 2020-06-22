1e12a949ee88c6f878f4c5ba3b52785a0537bb00
huisam <huisam@naver.com>
2020-06-22 22:30:31 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring REST Docs
+
+블로그에서 게시글로 활용되었던 자료입니다~!  
+[블로그 글](https://huisam.tistory.com/entry/RESTDocs)
+
---
+++ src/test/java/com/huisam/documentation/DocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huisam.documentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
